8daedd009cb1527a9ef1ad2aebd924bb71c17dcf
Dale Hemli <hemlidale@gmail.com>
2023-03-11 14:38:16 -0500

    somewhat there

--- scripts/validation.js
+++ scripts/validation.js
@@ -87,7 +87,7 @@ const enableValidation = (config) => {
 const validationConfig = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
+  submitButtonSelector: ".popup__save-button",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
