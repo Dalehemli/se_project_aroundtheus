493db1e1848658b63517ceabc0a4e92c5badd8df
Dale Hemli <hemlidale@gmail.com>
2023-03-13 10:32:31 -0400

    okthisisitone

--- scripts/validation.js
+++ scripts/validation.js
@@ -88,7 +88,7 @@ const validationConfig = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__save-button",
-  inactiveButtonClass: "popup__button_disabled",
+  inactiveButtonClass: "popup__button-disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
 };
