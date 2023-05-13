0961e162a31ad9e84d6996ddf75f0e1353422449
Dale Hemli <hemlidale@gmail.com>
2023-05-13 14:06:25 -0400

    working4

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -4,7 +4,7 @@ export default class PopupWithConfirmation extends Popup {
   constructor(popupSelector) {
     super({ popupSelector });
     this._popupForm = this._popupElement.querySelector(".popup__form");
-    this._saveButton = this._popupForm.querySelector(".popup__button");
+    this._saveButton = this._popupForm.querySelector(".popup__save-button");
   }
 
   setSubmitAction(action) {
